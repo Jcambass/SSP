36ec8a7fdac4a76bbde21d617cc3e24684f1034e
Joel Ambass <Jcambass@users.noreply.github.com>
2024-08-02 09:46:44 +0200

    fix ship image  path casing

--- js/script.js
+++ js/script.js
@@ -8,21 +8,21 @@ onload = function () {
 	imgBackground.src = 'images/background.png';
     imgPlayer.src = 'images/player.png';
 	imgExplosion.src = 'images/explosion.png';
-	
+
 	//Ships
 	imgBigShip.src = 'images/ships/bigShip.png';
 	imgSpaceCrusaderShip.src = 'images/ships/spaceCrusader.png';
 	imgDarkLordShip.src = 'images/ships/darkLord.png';
 	imgTrespasserShip.src = 'images/ships/trespasser.png';
 	imgHeavyCarrierShip.src = 'images/ships/heavyCarrier.png';
-	
+
 	//Projectiles
 	imgRatataProjectileUp.src = 'images/projectiles/ratataUp.png';
 	imgStompProjectileUp.src = 'images/projectiles/stompUp.png';
 	imgBlasterProjectileUp.src = 'images/projectiles/blasterUp.png';
 	imgGrimProjectileUp.src = 'images/projectiles/grimUp.png';
 	imgHammerProjectileUp.src = 'images/projectiles/hammerUp.png';
-	
+
 	imgRatataProjectileDown.src = 'images/projectiles/ratataDown.png';
 	imgStompProjectileDown.src = 'images/projectiles/stompDown.png';
 	imgBlasterProjectileDown.src = 'images/projectiles/blasterDown.png';
@@ -35,7 +35,7 @@ onload = function () {
 	imgStompWeapon.src = 'images/weapons/stompOMatic.png';
 	imgHammerWeapon.src = 'images/weapons/spaceHammer.png';
 	imgBlasterWeapon.src = 'images/weapons/spaceBlaster.png';
-	
+
 	//Obstacles
 	imgStar.src = 'images/obstacles/star.png';
 	imgPlanet01.src = 'images/obstacles/planet01.png';
@@ -45,22 +45,22 @@ onload = function () {
 
     //Create new Player
     player = new Player(imgPlayer, mainScreen.width / 2 - imgPlayer.width / 2, mainScreen.height - imgPlayer.height - 30);
-	
-	//New InputState 
+
+	//New InputState
 	inputState = new InputState();
-	
+
 	//Defining styles
     ctxMain.font = '32px Impact';
     ctxMain.fillStyle = "white";
-	
+
 	//Add some stars
 	for(var i = 0; i < 30; ++i) {
 		obstacles[obstacles.length] = new Obstacle(imgStar, Math.floor(Math.random() * (mainScreen.width - imgStar.width)), Math.floor(Math.random() * (mainScreen.height)), 0.43);
 	}
-	
+
 	msgDisplayTimeLeft = 170;
 	message = 'PROTECT EARTH AS LONG AS YOU CAN!!!';
-	
+
 	//Start the actual game
     StartLoop();
 }
@@ -73,13 +73,13 @@ var frameTime = 0, lastLoop = new Date, thisLoop;
 //GameLoop
 function StartLoop(){
 	bPlaying = true;
-	
+
 	//Set the updateInterval to 40fps
 	updateInterval = setInterval(Update, 1000/40);
 	enemySpawnInterval = setInterval(AddEnemy, nextEnemySpawn);
 	starSpawnInterval = setInterval(AddStar, nextStarSpawn);
 	planetSpawnInterval = setInterval(AddPlanet, nextPlanetSpawn);
-	
+
 	//Execute the loop
 	Loop();
 }
@@ -151,23 +151,23 @@ function StopLoop() {
 
 //BEGIN Update & Draw
 function Update() {
-	
+
 	//UPDATE ALL THE STUFF!
 	UpdateObstacles();
     UpdateEnemies();
 	player.Update();
 	UpdateProjectiles();
 	UpdateCollision();
-	
+
 	inputState.Reset();
-	
+
 	if(msgDisplayTimeLeft > 0)
 		--msgDisplayTimeLeft;
-	
+
 	//Check if the player has already won
 	if(score >= 8000)
 		DoWinGame();
-	
+
 	//FPS Stuff
 	var thisFrameTime = (thisLoop=new Date) - lastLoop;
 	frameTime+= (thisFrameTime - frameTime) / filterStrength;
@@ -189,12 +189,12 @@ function Draw() {
 		if (obstacles[i].speed >= 0.45)
 		obstacles[i].Draw();
 	}
-	
+
 	//Enemies
     for(var i = 0; i < enemies.length; ++i) {
         enemies[i].Draw();
     }
-	
+
 	//Projectiles
 	for(var i = 0; i < projectiles.length; ++i) {
         projectiles[i].Draw();
@@ -202,17 +202,17 @@ function Draw() {
 
     //The player
     player.Draw();
-	
+
 	//Animations
 	for(var i = 0; i < animations.length; ++i) {
         animations[i].Draw();
     }
-	
+
 	//Basic HUD. !VERY! basic
     ctxMain.fillText('Health: ' + player.health, mainScreen.width/2 - ctxMain.measureText('Health: ' + player.health).width/2, mainScreen.height - 12);
     ctxMain.fillText('Score: ' + score,10, 32+4);
 	ctxMain.fillText('Earth Health: ' + earthHealth, mainScreen.width/2 - ctxMain.measureText('Earth Health: ' + earthHealth).width/2, 4+32);
-	
+
 	//Display message if available
 	if(msgDisplayTimeLeft > 0) {
 		ctxMain.fillText(message, mainScreen.width/2-ctxMain.measureText(message).width/2,270);
@@ -224,7 +224,7 @@ function Draw() {
 //Function, which adds a new enemy to the enemies
 function AddEnemy() {
 	var tmpEnemy;
-	
+
 	var nextEnemyType = Math.floor(Math.random() * 100);
 	if(nextEnemyType < 45) {
 		tmpEnemy = new Trespasser(Math.floor(Math.random() * (mainScreen.width - imgTrespasserShip.width)), -imgTrespasserShip.height);
@@ -238,20 +238,20 @@ function AddEnemy() {
 	else {
 		tmpEnemy = new DarkLord(Math.floor(Math.random() * (mainScreen.width - imgDarkLordShip.width)), -imgDarkLordShip.height);
 	}
-	
+
 	//Assign a new random speed between the min and max speed of the enemy
 	var randomSpeed = Math.floor(Math.random() * tmpEnemy.maxSpeed);
     if (randomSpeed > tmpEnemy.minSpeed)
         tmpEnemy.speed = randomSpeed;
     else
         tmpEnemy.speed = tmpEnemy.minSpeed;
-	
+
 	//Set the enemys weaponPositionOffset centered
 	if(tmpEnemy.currentWeapon != null) {
 		tmpEnemy.currentWeapon.positionOffsetX = tmpEnemy.currentWeapon.texture.width/2;
 		tmpEnemy.currentWeapon.positionOffsetY = tmpEnemy.currentWeapon.texture.height/2;
 	}
-	
+
 	//Add the new enemy to the list
     enemies[enemies.length] = tmpEnemy;
 
@@ -266,9 +266,9 @@ function AddEnemy() {
 
 function AddStar() {
 	var tmpStar = new Star(Math.floor(Math.random() * (mainScreen.width - imgStar.width)), -imgStar.height);
-	
+
 	obstacles[obstacles.length] = tmpStar;
-	
+
 	//Set the next spawn interval
 	clearInterval(starSpawnInterval);
     nextStarSpawn = Math.floor(Math.random() * maxStarSpawnDelay);
@@ -281,7 +281,7 @@ function AddStar() {
 function AddPlanet() {
 	var tmpPlanet;
 	var planetType = Math.floor(Math.random() * 4);
-	
+
 	if(planetType == 1) {
 		tmpPlanet = new Planet(imgPlanet01, Math.floor(Math.random() * (mainScreen.width - imgPlanet01.width)), -imgPlanet01.height);
 	}
@@ -295,7 +295,7 @@ function AddPlanet() {
 		tmpPlanet = new Planet(imgPlanet04, Math.floor(Math.random() * (mainScreen.width - imgPlanet04.width)), -imgPlanet04.height);
 	}
 	obstacles[obstacles.length] = tmpPlanet;
-	
+
 	//Set the next spawn interval
 	clearInterval(planetSpawnInterval);
     nextPlanetSpawn = Math.floor(Math.random() * maxPlanetSpawnDelay);
